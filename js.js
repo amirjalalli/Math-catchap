05c8d409a93e3486b8ea417fad2cd4eee1152940
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-28 16:42:34 +0330

    Math.catchap

---
+++ js.js
@@ -0,0 +1,15 @@
+var num1 = String(Math.floor(Math.random() * 100000));
+// var num2 = String(Math.floor(Math.random() * 10))
+// var num3 = String(Math.floor(Math.random() * 10))
+// var num4 = String(Math.floor(Math.random() * 10))
+// var num5 = String(Math.floor(Math.random() * 10))
+
+
+// var caphai = (num1 + num2 + num3 +num4 + num5);
+// console.log(caphai);
+alert(num1)
+
+
+// console.log(Math.pow(2 , 3))
+// console.log(Math.abs(-12))
+// console.log(Math.max(10 , 13 , 8))
